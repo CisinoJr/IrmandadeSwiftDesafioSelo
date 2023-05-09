1b1c7f03892065530f1661d1329e876c0f56ed26
Cisino Junior <cisino.gomes@stone.com.br>
2023-05-09 18:17:15 -0300

    Initial Commit

---
+++ IrmandadeSwiftDesafioSelo/IrmandadeSwiftDesafioSeloApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrmandadeSwiftDesafioSeloApp.swift
+//  IrmandadeSwiftDesafioSelo
+//
+//  Created by Cisino Junior on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct IrmandadeSwiftDesafioSeloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
